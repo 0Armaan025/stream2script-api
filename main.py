56e566cfc3ee93b0e2625911bc481d74a9f8d30c
Armaan <armaan33000@gmail.com>
2024-06-22 11:48:48 +0530

    this works

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 # INIT
+from flask import Flask
+from pytube import YouTube
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    download_video('https://www.youtube.com/watch?v=TK4N5W22Gts')
+    return 'hi arcade people!'
+
+
+def download_video(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
